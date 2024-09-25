a7c2e48131b25fe2164712ceb826515d1ddc9819
Ivan <eyeonex.info@gmail.com>
2024-09-25 12:49:32 +0300

    testing burger menu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import burgerIcon from "../../assets/images/icons/burger_menu.svg";
-import './BurgerMenu.css'
+import "./BurgerMenu.css";
+import { Link } from "react-router-dom";
+import Logo from "../Logo/Logo";
 
 const BurgerMenu = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuActive((prevState) => !prevState);
+  };
+
   return (
-    <div className="burger__container">
-      <button className="burger__button"></button>
-    </div>
+    <>
+      <div className="burger__container">
+        <button className="burger__button" onClick={handleMenuToggle}></button>
+      </div>
+      {isMenuActive && (
+        <div className="burger__menuContainer">
+          <div className="overlay" onClick={handleMenuToggle}></div>
+          <div className="burger__menu">
+            <div className="menu__titleContainer">
+              <h3 className="menu__title">Menu</h3>
+              <button
+                className="menu__closeButton"
+                onClick={handleMenuToggle}
+              ></button>
+            </div>
+            {/* <a className="menu__logo" href="/">
+              <img
+                className="menu__logoImg"
+                src="/src/assets/images/logo-gray.svg"
+                alt="лого"
+              />
+            </a> */}
+            <div className="burger__links">
+              <Link className="burger__link" to={"/"}>
+                Home
+              </Link>
+              <a
+                className="burger__link"
+                href="#contacts__id"
+                onClick={handleMenuToggle}
+              >
+                Contacts
+              </a>
+              <Link className="burger__link" to={"/excursions"}>
+                Excursions
+              </Link>
+              <Link className="burger__link" to={"/tours"}>
+                Tour
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
