30040ce83ba41aac3dce559efa5cb846239155c3
Ivan <eyeonex.info@gmail.com>
2024-09-25 17:54:04 +0300

    change insta size and mail

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -6,8 +6,8 @@ const Contacts = () => {
   return (
     <section className="contacts" id="contacts__id">
       <h3 className="contacts__title">Contact me</h3>
-      <a className="contacts__link" href="mailto:mintfrau@gmail.com">
-      mintfrau@gmail.com
+      <a className="contacts__link" href="mailto:Tokyona.tours@gmail.com">
+        Tokyona.tours@gmail.com
       </a>
       <Social />
     </section>
