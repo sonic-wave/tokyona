3490967502004eddd72d0301e9d2da881fd4c2ea
Ivan <eyeonex.info@gmail.com>
2024-10-01 16:19:13 +0300

    change scrolltotop for firefox

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,3 +1,29 @@
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+
+// const ScrollToTop = () => {
+//   const { pathname, hash } = useLocation();
+
+//   useEffect(() => {
+//     if (hash) {
+//       const element = document.querySelector(hash);
+//       if (element) {
+//         element.scrollIntoView({ behavior: "smooth" });
+//       }
+//     } else {
+//       window.scrollTo({
+//         top: 0,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//     }
+//   }, [pathname, hash]);
+
+//   return null;
+// };
+
+// export default ScrollToTop;
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -5,18 +31,22 @@ const ScrollToTop = () => {
   const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    if (hash) {
-      const element = document.querySelector(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
+    const scrollTimeout = setTimeout(() => {
+      if (hash) {
+        const element = document.querySelector(hash);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      } else {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
       }
-    } else {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-      });
-    }
+    }, 100); 
+
+    return () => clearTimeout(scrollTimeout);
   }, [pathname, hash]);
 
   return null;
